e8277551fa165805a93c42c712303e836428e01c
daniel_peck <daniel.syre.peck@gmail.com>
2025-08-20 12:18:38 -0700

    completed the player class and grouped it into drawable and updatable

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -7,7 +7,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
     dt = 0
 
 
@@ -16,11 +23,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        updatable.update(dt)
+
         screen.fill("black")
-        
-        # draw player to screen
-        player.update(dt)
-        player.draw(screen)
+
+        for obj in drawable: 
+            obj.draw(screen)
+
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
